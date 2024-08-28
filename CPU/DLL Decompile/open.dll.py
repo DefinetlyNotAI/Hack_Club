12fe1ae5df3bc013d23f028891bf5306f4e0c8a3
Shahm Najeeb <Nirt_12023@outlook.com>
2024-08-28 16:11:13 +0300

    Added error checks and more verbose logging

--- CPU/DLL Decompile/open.dll.py
+++ CPU/DLL Decompile/open.dll.py
@@ -2,20 +2,38 @@ import os
 import sys
 
 # Get current username
-username = os.environ.get("USERNAME")
+try:
+    username = os.environ.get("USERNAME")
+except Exception as e:
+    print(f"An error occured fetching your username: {e}")
+    exit(e)
 
 # Construct path
 path = f"C:\\Users\\{username}\\AppData\\Local\\JetBrains\\Installations\\dotPeek242\\dotPeek64.exe"
 
-# Check if path exists
-if os.path.exists(path):
-    # Get DLL path from command line arguments
-    dll_path = sys.argv[1]
+if sys.argv[1] is None:
+    print("You need to include the dll path AFTER typing `python open.dll.py`")
+    exit("No file parsed")
+elif not sys.argv[1].endswith(".dll")
+    print("The path MUST lead to a `.dll` file, the file path given does not!")
+    print(sys.argv[1])
+    exit("Non DLL file parsed")
 
-    # Construct full command
-    command = f'"{path}" "{dll_path}"'
-
-    # Execute command
-    os.system(command)
-else:
-    print(f"Path does not exist: {path}")
+try:
+    # Check if path exists
+    if os.path.exists(path):
+        # Get DLL path from command line arguments
+        dll_path = sys.argv[1]
+    
+        # Construct full command
+        command = f'"{path}" "{dll_path}"'
+    
+        # Execute command
+        os.system(command)
+    else:
+        print(f"dotPeek does not exist: {path}")
+        print("Please make sure you have it installed in that directory")
+        exit("dotPeek does not exist")
+except Exception as e:
+    print(f"An error occured executing the script dotPeek64: {e}")
+    exit(e)
